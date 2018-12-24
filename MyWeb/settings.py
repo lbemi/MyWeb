f11ccdaceeb8ad8311e05b5dc49aad7e875acd87
wjl <934366858@qq.com>
2018-12-24 17:52:28 +0800

    update some

--- MyWeb/settings.py
+++ MyWeb/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from MyWeb import MW
+
+import MW
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

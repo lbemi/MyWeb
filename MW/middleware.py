050ec6d59d4e6d569af9e816a23bd25ab8e520e3
wjl <934366858@qq.com>
2018-12-24 18:00:53 +0800

    1111

---
+++ MW/middleware.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponseForbidden
+
+class AaccessRestrictionMiddleware: #拦截IP
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.wite_ip = ['127.0.0.1','192.168.7.132']
+
+
+    def __call__(self, request):
+        ip = request.META['REMOTE_ADDR']
+        if ip not in self.wite_ip:
+            return HttpResponseForbidden('该IP禁止访问！！')
+        response = self.get_response(request)
+
+        return response
